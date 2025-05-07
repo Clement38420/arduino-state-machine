2fa491978a12b575963ff6bb0d8812a3c02527db
Clement Charbonnel <c.charbo38@gmail.com>
2025-05-07 19:29:55 +0200

    DEFINE TRANSITION STRUCT
    Contains current state, the condition to verify, an optional action to execute before changing the state, and the next state

--- fsm.h
+++ fsm.h
@@ -2,9 +2,18 @@
 
 typedef void (*ActionFunc)();
 
+typedef bool (*Condition)();
+
 struct State {
   ActionFunc on_entry_action;
   ActionFunc periodic_action;
   ActionFunc on_exit_action;
   char name[20];
+};
+
+struct Transition {
+  State *current_state;
+  Condition condition;
+  ActionFunc action;
+  State *next_state;
 };
