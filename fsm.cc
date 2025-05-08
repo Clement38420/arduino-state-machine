c2beb41507823c57fe2c94287d640f3fbbb4bed8
Clement Charbonnel <c.charbo38@gmail.com>
2025-05-08 14:52:50 +0200

    IMPLEMENTED THE FSM CLASS
    The FSM class is used for updating the current state of the machine by processing transitions.

--- fsm.cc
+++ fsm.cc
@@ -1,1 +1,35 @@
 #include <fsm.h>
+
+FSM::FSM(State *initial_state, Transition transitions[]) {
+  current_state_ = initial_state;
+  transitions_ = transitions;
+  number_of_transitions_ = sizeof(transitions) / sizeof(Transition);
+}
+
+State *FSM::ProcessStateTransitions() {
+  for (int i = 0; i < number_of_transitions_; i++) {
+    const Transition &t = transitions_[i];
+    if (t.current_state == current_state_ && t.condition()) {
+      ApplyTransition(t);
+      break;
+    }
+  }
+  return current_state_;
+}
+
+void FSM::ApplyTransition(const Transition &t) {
+  if (current_state_->on_exit_action) {
+    current_state_->on_exit_action;
+  }
+
+  if (t.action) {
+    t.action();
+  }
+
+  current_state_ = t.next_state;
+
+  if (current_state_->on_entry_action) {
+    current_state_->on_entry_action;
+  }
+}
+
--- fsm.h
+++ fsm.h
@@ -17,3 +17,17 @@ struct Transition {
   ActionFunc action;
   State *next_state;
 };
+
+class FSM {
+  public:
+    FSM(State *initial_state, Transition *transitions);
+    State *GetCurrentState() const { return current_state_; };
+    State *ProcessStateTransitions();
+
+  private:
+    State *current_state_;
+    Transition *transitions_;
+    int number_of_transitions_;
+
+    void ApplyTransition(const Transition &t);
+};
